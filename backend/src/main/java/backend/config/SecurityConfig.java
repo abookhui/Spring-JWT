0287cb9210a98cd228773ad5b459bd87a235ee7e
HanGwonhui <jana32496@gmail.com>
2025-02-12 19:56:24 +0900

    cors

--- backend/src/main/java/backend/config/SecurityConfig.java
+++ backend/src/main/java/backend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ package backend.config;
 import backend.jwt.JwtFilter;
 import backend.jwt.JwtUtil;
 import backend.jwt.LoginFilter;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,6 +17,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -62,6 +67,26 @@ public class SecurityConfig {
         // 세션 STATELESS
         http. sessionManagement((session) -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        http
+                .cors((corsCustomizer) -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration config = new CorsConfiguration();
+
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+
+                        config.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return config;
+                    }
+                }));
 
         return http.build();
     }

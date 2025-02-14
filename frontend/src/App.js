15866749949f4bc62163652d255e4594d6e5d968
HanGwonhui <jana32496@gmail.com>
2025-02-14 18:09:11 +0900

    loginPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./LoginPage";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
-
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home/> }/>
+                <Route path="/login" element={<Login />}/>
+            </Routes>
+        </Router>
     </div>
   );
 }
